b2e68714e73312e4294c1190e69e6e3283913553
Ondra Kučera <ondrej.kucera@centrum.cz>
2023-09-09 11:56:13 +0200

    Add alza_cad_naive.user.js

---
+++ alza_cad_naive.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name        Alza in CAD (naïve version)
+// @namespace   Ondra
+// @match       https://www.alza.cz/*
+// @grant       none
+// @version     1.0
+// @author      Ondra Kučera
+// @description Zobrazí ceny na Alze v kanadských dolarech (ve skutečnosti funguje pouze na detailu zboží)
+// ==/UserScript==
+
+const CAD = 16.5;
+const PRICE_ELEMENT_SELECTORS = ".price-container__price, .price-box__price, .price";
+
+const getPriceNumber = (priceString) => Number(priceString.replace(/\s/g, "").slice(0, -2));
+const czkToCad = (priceInCzk) => Math.round(priceInCzk / CAD);
+
+document.querySelectorAll(PRICE_ELEMENT_SELECTORS).forEach((priceElement) => {
+	priceElement.textContent = `${czkToCad(getPriceNumber(priceElement.textContent))} CAD`;
+});
