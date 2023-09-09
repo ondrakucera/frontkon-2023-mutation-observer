9573c706fe100281d88fe126dfe74cb0a215e754
Ondra Kučera <ondrej.kucera@centrum.cz>
2023-09-09 11:56:21 +0200

    Add alza_cad.user.js

---
+++ alza_cad.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name        Alza in CAD
+// @namespace   Ondra
+// @match       https://www.alza.cz/*
+// @grant       none
+// @version     1.0
+// @author      Ondra Kučera
+// @description Zobrazí ceny na Alze v kanadských dolarech
+// ==/UserScript==
+
+const CAD = 16.5;
+const PRICE_ELEMENT_SELECTORS = ".price-container__price, .price-box__price";
+
+const getPriceNumber = (priceString) => Number(priceString.replace(/\s/g, "").slice(0, -2));
+const czkToCad = (priceInCzk) => Math.round(priceInCzk / CAD);
+const changePriceToCad = (priceElement) => {
+	priceElement.textContent = `${czkToCad(getPriceNumber(priceElement.textContent))} CAD`;
+};
+
+document.body.querySelectorAll(PRICE_ELEMENT_SELECTORS).forEach((priceElement) => {
+	changePriceToCad(priceElement);
+});
+
+const observer = new MutationObserver((mutationRecords) => {
+	mutationRecords.forEach((mutationRecord) => {
+		mutationRecord.addedNodes.forEach((node) => {
+			if (node.nodeType === Node.ELEMENT_NODE) {
+				node.querySelectorAll(PRICE_ELEMENT_SELECTORS).forEach((priceElement) => {
+					changePriceToCad(priceElement);
+				});
+			}
+		});
+	});
+});
+observer.observe(document.body, { childList: true, subtree: true });
