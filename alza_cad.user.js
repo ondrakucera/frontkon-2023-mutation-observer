86169d6983b54261161cb1339a6cb7783afaf6d9
Ondra Kučera <ondrej.kucera@centrum.cz>
2023-10-12 09:24:57 +0200

    Update price element selectors

--- alza_cad.user.js
+++ alza_cad.user.js
@@ -9,7 +9,7 @@
 // ==/UserScript==
 
 const CAD = 16.5;
-const PRICE_ELEMENT_SELECTORS = ".price-container__price, .price-box__price, .price-detail__without-vat-value";
+const PRICE_ELEMENT_SELECTORS = ".price-container__price, .price-box__price, .price-detail__without-vat-value, .price-value";
 
 const getPriceNumber = (priceString) => Number(priceString.replace(/\s/g, "").slice(0, -2));
 const czkToCad = (priceInCzk) => Math.round(priceInCzk / CAD);
